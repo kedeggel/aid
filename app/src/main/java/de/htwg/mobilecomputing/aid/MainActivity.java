433dfb1f4a96c11fa6f79ded8854ddebc3745411
Kevin Deggelmann <kevin.deggelmann@web.de>
2018-10-30 11:07:05 +0100

    Add class structure and settings

--- app/src/main/java/de/htwg/mobilecomputing/aid/MainActivity.java
+++ app/src/main/java/de/htwg/mobilecomputing/aid/MainActivity.java
@@ -1,7 +1,9 @@
 package de.htwg.mobilecomputing.aid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void openSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    // TODO
+    public void openLiveStream(View view) {}
+
+    // TODO
+    public void openGalery(View view) {}
 }
