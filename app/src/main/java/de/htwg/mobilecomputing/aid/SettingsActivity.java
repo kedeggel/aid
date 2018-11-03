749c79eb3dac189a6dcd42c40e1b944cd5f65947
Kevin Deggelmann <kevin.deggelmann@web.de>
2018-11-03 18:22:38 +0100

    Implement settings
    Use external fix to solve problem with input type

--- app/src/main/java/de/htwg/mobilecomputing/aid/SettingsActivity.java
+++ app/src/main/java/de/htwg/mobilecomputing/aid/SettingsActivity.java
@@ -1,28 +1,16 @@
 package de.htwg.mobilecomputing.aid;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
+        getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
-    }
 
-    public static class SettingsFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.preferences);
-        }
     }
 }
